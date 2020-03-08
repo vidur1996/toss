1d082167b9c72df1b2351c39a111facaeca8a24d
vidurshan96 <vidurshan1996@gmail.com>
2020-03-08 12:40:41 +0530

    added ad

--- app/src/main/java/com/vidu/toss/MainActivity.java
+++ app/src/main/java/com/vidu/toss/MainActivity.java
@@ -10,11 +10,19 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
+    private AdView mAdView;
 
     public void blink(View view){
         ImageView imagev = (ImageView)findViewById(R.id.imagev);
@@ -30,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+
 
         final ImageView imagev =  (ImageView) findViewById(R.id.imagev) ;
 
@@ -60,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         text.setText(outs);
                     }
-                }, 1800);
+                }, 800);
 
             }
         });
